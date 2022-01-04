1aa1cd50e47362de115a5b414ac048c5fbdeddd8
Nicolas Berthel <contact@nicolasberthel.fr>
2022-01-04 14:36:25 +0100

    Update release version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pybecker",
-    version="1.0.5",
+    version="1.0.6",
     author="Nicolas Berthel",
     author_email="contact@nicolasberthel.fr",
     install_requires=['pyserial>=3.4'],
